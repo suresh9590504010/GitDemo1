e4d2151246fa5bf353e836959e2b7b48981f489f
kishan das <kishan das@LAPTOP-8LDNGHG4>
2019-12-27 07:29:45 +0530

    This My First Commit

---
+++ GitDemo/src/com/test/Test1.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		int i=10;
+		int p=90;
+		int n=i+p;
+		System.out.println("Addition is "+ n);
+
+	}
+
+}
